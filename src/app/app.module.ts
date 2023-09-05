8e67566ec87981254acfaf661af23a9517335f0d
Bernardo Fusco <blfuscomail@gmail.com>
2023-09-05 13:11:37 -0300

    Commit dev-angular (commit on 'Master')

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RodapeComponent } from './rodape/rodape.component';
+import { LateralComponent } from './lateral/lateral.component';
+import { HomeComponent } from './home/home.component';
+import { ProdutoComponent } from './produto/produto.component';
+import { NovProdComponent } from './nov-prod/nov-prod.component';
+
+import { ProdutoService } from './produto.service';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { LoginComponent } from './login/login.component';
+import { LoginService } from './login.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RodapeComponent,
+    LateralComponent,
+    HomeComponent,
+    ProdutoComponent,
+    NovProdComponent,
+    LoginComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule, AppRoutingModule, CommonModule, FormsModule],
+  providers: [ProdutoService, LoginService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
